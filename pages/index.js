e02e0b6ba2f2dfa602504da64e7ff6847a2fa0b1
klesio02 <klesio.k9@gmail.com>
2021-02-04 01:33:25 -0300

    was a created database and added react components

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import styled from "styled-components";
+import db from "../db.json";
 
-const Title = styled.h1`
+/* const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
+  color: ${({ theme }) => theme.colors.secundary};
+`; */
 
 /* function Title(props) {
   // propcidades
-  return <h1>{props.children}</h1>;
+  return (
+    <h1>
+      {props.children})
+    </h1>;
 } */
 
+
+
+
+const BackgroundImage = styled.div`
+  background-image : url(${db.bg});
+  flex:1;
+  background-size: cover;
+  background-position: center;
+
+`;
+
 export default function Home() {
-  return <Title>My page</Title>;
+  return <BackgroundImage>Fundação</BackgroundImage>;
 }
